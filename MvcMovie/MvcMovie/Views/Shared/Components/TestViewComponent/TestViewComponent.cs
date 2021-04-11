6c6dabe3dc2493ad3665a8cd835f68c17cb41702
huuchuongdo <dhchuong9@gmail.com>
2021-04-11 13:15:37 -0700

    first commit for mvcMovie

---
+++ MvcMovie/MvcMovie/Views/Shared/Components/TestViewComponent/TestViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Views.Shared.Components.TestViewComponent
+{
+    public class TestViewComponent: ViewComponent
+    {
+        public string Invoke()
+        {
+            return "this is testing from view component";
+        }
+    }
+}
